9882f8b6d4456cb779f06d462fd8903fca082047
Rhema <rehubdevelopers@gmail.com>
2026-01-25 04:00:44 +0100

    added scopes of reading gmail 2

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -71,7 +71,7 @@ export function SignupForm({ className, ...props }: React.ComponentProps<"form">
     setLoading(false);
   }, 1000);
 };
-w
+
 
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props} onSubmit={handleSignup}>
